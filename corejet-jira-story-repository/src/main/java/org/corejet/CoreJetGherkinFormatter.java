b50c984689c6d9a0608b4e007990374ccc76e343
mahsubramanian <mahsubramanian@deloitte.co.uk>
2013-01-04 11:45:13 +0000

    Changes introduced: 1. Updated gherkin library version from 2.4.16 to 2.11.5 2. Incremented POM version number to release version 0.20 3. Cleaned up POM to remove duplicate entries in sub-module POMs

--- corejet-jira-story-repository/src/main/java/org/corejet/CoreJetGherkinFormatter.java
+++ corejet-jira-story-repository/src/main/java/org/corejet/CoreJetGherkinFormatter.java
@@ -1,10 +1,5 @@
 package org.corejet;
 
-import java.util.List;
-import java.util.Map;
-
-import org.corejet.model.Story;
-
 import gherkin.formatter.Formatter;
 import gherkin.formatter.model.Background;
 import gherkin.formatter.model.Examples;
@@ -13,73 +8,94 @@ import gherkin.formatter.model.Scenario;
 import gherkin.formatter.model.ScenarioOutline;
 import gherkin.formatter.model.Step;
 
+import java.util.List;
+import java.util.Map;
+
+import org.corejet.model.Story;
+
 public class CoreJetGherkinFormatter implements Formatter {
 
-	private final Story story;
-	private org.corejet.model.Scenario currentScenario;
-	private Map<String,Double> lastModifiedStepMap;
+    private final Story story;
+    private org.corejet.model.Scenario currentScenario;
+    private Map<String, Double> lastModifiedStepMap;
 
-	public CoreJetGherkinFormatter(Story story) {
-		this.story = story;
-	}
+    public CoreJetGherkinFormatter(Story story) {
+        this.story = story;
+    }
 
-	public void uri(String uri) {
-		
-	}
+    public void uri(String uri) {
 
-	public void feature(Feature feature) {
-		
-	}
+    }
 
-	public void background(Background background) {
-		throw new UnsupportedOperationException("Background not supported yet");
-	}
+    public void feature(Feature feature) {
 
-	public void scenario(Scenario scenario) {
-		currentScenario = new org.corejet.model.Scenario();
-		story.getScenarios().add(currentScenario);
-		currentScenario.setParentStory(story);
-		
-		currentScenario.setName(scenario.getName());
-	}
+    }
 
-	public void scenarioOutline(ScenarioOutline scenarioOutline) {
-		throw new UnsupportedOperationException("Scenario Outline not supported yet");
-	}
+    public void background(Background background) {
+        throw new UnsupportedOperationException("Background not supported yet");
+    }
 
-	public void examples(Examples examples) {
-		throw new UnsupportedOperationException("Examples not supported yet");
-	}
+    public void scenario(Scenario scenario) {
+        currentScenario = new org.corejet.model.Scenario();
+        story.getScenarios().add(currentScenario);
+        currentScenario.setParentStory(story);
 
-	public void step(Step step) {
-		String keyword = step.getKeyword().trim().toLowerCase();
-		
-		if ("given".equals(keyword)) {
-			currentScenario.getGivens().put(step.getName(),0.0);
-			lastModifiedStepMap = currentScenario.getGivens();
-		}
-		
-		if ("when".equals(keyword)) {
-			currentScenario.getWhens().put(step.getName(),0.0);
-			lastModifiedStepMap = currentScenario.getWhens();
-		}
-		
-		if ("then".equals(keyword)) {
-			currentScenario.getThens().put(step.getName(),0.0);
-			lastModifiedStepMap = currentScenario.getThens();
-		}
-		
-		if ("and".equals(keyword)) {
-			lastModifiedStepMap.put(step.getName(),0.0);
-		}
-	}
+        currentScenario.setName(scenario.getName());
+    }
 
-	public void eof() {
-		
-	}
+    public void scenarioOutline(ScenarioOutline scenarioOutline) {
+        throw new UnsupportedOperationException("Scenario Outline not supported yet");
+    }
 
-	public void syntaxError(String state, String event, List<String> legalEvents, String uri, int line) {
-		throw new CoreJetStoryParsingException(String.format("State: %s, Event: %s, LegalEvents: %s, Line: %d", state, event, legalEvents.toString(), line));
-	}
+    public void examples(Examples examples) {
+        throw new UnsupportedOperationException("Examples not supported yet");
+    }
+
+    public void step(Step step) {
+        String keyword = step.getKeyword().trim().toLowerCase();
+
+        if ("given".equals(keyword)) {
+            currentScenario.getGivens().put(step.getName(), 0.0);
+            lastModifiedStepMap = currentScenario.getGivens();
+        }
+
+        if ("when".equals(keyword)) {
+            currentScenario.getWhens().put(step.getName(), 0.0);
+            lastModifiedStepMap = currentScenario.getWhens();
+        }
+
+        if ("then".equals(keyword)) {
+            currentScenario.getThens().put(step.getName(), 0.0);
+            lastModifiedStepMap = currentScenario.getThens();
+        }
+
+        if ("and".equals(keyword)) {
+            lastModifiedStepMap.put(step.getName(), 0.0);
+        }
+    }
+
+    public void eof() {
+
+    }
+
+    public void syntaxError(String state, String event, List<String> legalEvents, String uri, int line) {
+        throw new CoreJetStoryParsingException(String.format("State: %s, Event: %s, LegalEvents: %s, Line: %d", state,
+            event, legalEvents.toString(), line));
+    }
+
+    public void close() {
+        // TODO Auto-generated method stub
+
+    }
+
+    public void done() {
+        // TODO Auto-generated method stub
+
+    }
+
+    public void syntaxError(String arg0, String arg1, List<String> arg2, String arg3, Integer arg4) {
+        // TODO Auto-generated method stub
+
+    }
 
 }
--- corejet-jira-story-repository/src/main/java/org/corejet/GherkinScenarioProcessor.java
+++ corejet-jira-story-repository/src/main/java/org/corejet/GherkinScenarioProcessor.java
@@ -1,55 +1,55 @@
 package org.corejet;
 
+import gherkin.formatter.Formatter;
+import gherkin.lexer.LexingError;
+import gherkin.parser.Parser;
+
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
-
-import gherkin.GherkinParser;
-import gherkin.formatter.Formatter;
-import gherkin.lexer.LexingError;
 
 import org.corejet.model.Story;
 
 public class GherkinScenarioProcessor {
 
-	private static final String COMMENT_MARKER = "//";
+    private static final String COMMENT_MARKER = "//";
 
-	public void parse(String sourceText, Story storyToPopulate) {
-		Formatter corejetFormatter = new CoreJetGherkinFormatter(storyToPopulate);
-		GherkinParser gherkinParser = new GherkinParser(corejetFormatter);
+    public void parse(String sourceText, Story storyToPopulate) {
+        Formatter corejetFormatter = new CoreJetGherkinFormatter(storyToPopulate);
+        Parser gherkinParser = new Parser(corejetFormatter);
 
-		String filteredSourceText = "";
+        String filteredSourceText = "";
 
-		Map<String, String> gherkinReplacements = new HashMap<String, String>();
+        Map<String, String> gherkinReplacements = new HashMap<String, String>();
 
-		gherkinReplacements.put("SCENARIO:", "Scenario:");
-		gherkinReplacements.put("GIVEN", "Given");
-		gherkinReplacements.put("WHEN", "When");
-		gherkinReplacements.put("AND", "And");
-		gherkinReplacements.put("THEN", "Then");
+        gherkinReplacements.put("SCENARIO:", "Scenario:");
+        gherkinReplacements.put("GIVEN", "Given");
+        gherkinReplacements.put("WHEN", "When");
+        gherkinReplacements.put("AND", "And");
+        gherkinReplacements.put("THEN", "Then");
 
-		// remove code blocks and comments
-		for (String line :sourceText.replaceAll("\\{code\\}", "").split("\n")){
-			line = replaceFirst(line, gherkinReplacements);
-			if (!line.trim().startsWith(COMMENT_MARKER)){
-				filteredSourceText += line + "\n";
-			}
-		}
-		try {
-			gherkinParser.parse("Feature: " + storyToPopulate.getTitle() + "\n" + filteredSourceText, "", 0);
-		} catch (LexingError e) {
-			throw new CoreJetStoryParsingException("Error parsing story with ID:" + storyToPopulate.getId(), e);
-		}
+        // remove code blocks and comments
+        for (String line : sourceText.replaceAll("\\{code\\}", "").split("\n")) {
+            line = replaceFirst(line, gherkinReplacements);
+            if (!line.trim().startsWith(COMMENT_MARKER)) {
+                filteredSourceText += line + "\n";
+            }
+        }
+        try {
+            gherkinParser.parse("Feature: " + storyToPopulate.getTitle() + "\n" + filteredSourceText, "", 0);
+        } catch (LexingError e) {
+            throw new CoreJetStoryParsingException("Error parsing story with ID:" + storyToPopulate.getId(), e);
+        }
 
-	}
+    }
 
-	private String replaceFirst(String source, Map<String, String> replacements){
-		for (Entry<String, String> replacement: replacements.entrySet()){
-			if (source.trim().startsWith(replacement.getKey())){
-				source = source.replace(replacement.getKey(),replacement.getValue());
-			}
-		}
-		return source;
-	}
+    private String replaceFirst(String source, Map<String, String> replacements) {
+        for (Entry<String, String> replacement : replacements.entrySet()) {
+            if (source.trim().startsWith(replacement.getKey())) {
+                source = source.replace(replacement.getKey(), replacement.getValue());
+            }
+        }
+        return source;
+    }
 
 }
